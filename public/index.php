e073d72dd60123982fa346754c457eb1cc2f0f2b
Shabrina Amalia Safaana <shabrinaamalia860@gmail.com>
2025-10-24 01:29:15 +0700

    nyoba config

--- public/index.php
+++ public/index.php
@@ -17,4 +17,7 @@ require __DIR__.'/../vendor/autoload.php';
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
+if (class_exists(\Laravel\Pail\PailServiceProvider::class)) {
+    $app->register(\Laravel\Pail\PailServiceProvider::class);
+}
 $app->handleRequest(Request::capture());
